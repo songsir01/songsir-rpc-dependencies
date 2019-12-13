e755fdc4508a2e3296c8f5fd51bd57192dbc2c6c
songsir01 <1126563633@qq.com>
2019-12-13 15:25:17 +0800

    create new rpc dependencies

---
+++ src/main/java/com/songsir/rpc/App.java
@@ -0,0 +1,15 @@
+package com.songsir.rpc;
+
+/**
+ * @PackageName com.songsir.rpc
+ * @ProjectName songsir-rpc-dependencies
+ * @Author: SongYapeng
+ * @Date: Create in 15:21 2019/12/13
+ * @Description:
+ * @Copyright Copyright (c) 2019, songsir01@163.com All Rights Reserved.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
